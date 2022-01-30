d49ba420442fe5667ca308e69b08c19eaa059b3d
codecoco <88923210+codecocos@users.noreply.github.com>
2022-01-30 15:56:45 +0900

    naivecoin_JS_chapter4_5 : 5가 잘 안되서 다시해봄

---
+++ naivecoinJS/chapter4_5/p2p.js
@@ -0,0 +1,217 @@
+const WebSocket = require('ws')
+const { addBlockToChain, getLatestBlock, isValidBlockStructure, replaceChain, getBlockchain, handleReceivedTransaction } = require('./blockchain');
+const { getTransactionPool } = require('./transactionPool');
+
+const sockets = [];
+
+const MessageType = {
+    QUERY_LATEST: 0,
+    QUERY_ALL: 1,
+    RESPONSE_BLOCKCHAIN: 2,
+    //ch5
+    QUERY_TRANSACTION_POOL: 3,
+    RESPONSE_TRANSACTION_POOL: 4
+}
+
+
+const initP2PServer = (p2pPort) => {
+    const server = new WebSocket.Server({ port: p2pPort });
+    server.on('connection', (ws) => {
+        console.log("\n@@@@웹소켓서버 서버 온@@@@@\n");
+        initConnection(ws);
+    });
+    console.log('listening websocket p2p port on: ' + p2pPort);
+};
+
+const getSockets = () => sockets;
+
+const initConnection = (ws) => {
+    console.log('4. 인잇컨넥션 진입 - 소켓배열에 푸쉬됨\n');
+    sockets.push(ws);
+    initMessageHandler(ws);
+    initErrorHandler(ws);
+    write(ws, queryChainLengthMsg());
+
+    //ch5
+    // query transactions pool only some time after chain query
+    setTimeout(() => {
+        broadcast(queryTransactionPoolMsg());
+    }, 500);
+};
+
+const JSONToObject = (data) => {
+    try {
+        return JSON.parse(data);
+    } catch (e) {
+        console.log(e);
+        return null;
+    }
+};
+
+const initMessageHandler = (ws) => {
+    console.log('5. 메세지 핸들러 진입\n');
+    //ch5
+
+    //chapter4
+    ws.on('message', (data) => {
+        console.log('\n6. 메시지 핸들러 속 메세지 확인\n');
+        //ch5
+        try {
+            //버퍼로 받은 데이터 제이슨 형식으로 파서
+            const message = JSONToObject(data);
+
+            if (message === null) {
+                console.log('could not parse received JSON message: ' + data);
+                return;
+            }
+            console.log('Received message: %s', JSON.stringify(message));
+
+            switch (message.type) {
+                case MessageType.QUERY_LATEST:
+                    console.log("\n7. 메세지타입 : QUERY_LATEST");
+                    write(ws, responseLatestMsg());
+                    break;
+                case MessageType.QUERY_ALL:
+                    console.log("\n7. 메세지타입 : QUERY_ALL");
+                    write(ws, responseChainMsg());
+                    break;
+                case MessageType.RESPONSE_BLOCKCHAIN:
+                    console.log("\n7. 메세지타입 : RESPONSE_BLOCKCHAIN");
+                    const receivedBlocks = JSONToObject(message.data);
+                    if (receivedBlocks === null) {
+                        console.log('invalid blocks received: %s', JSON.stringify(message.data));
+                        break;
+                    }
+                    handleBlockchainResponse(receivedBlocks);
+                    break;
+
+                //ch5
+                case MessageType.QUERY_TRANSACTION_POOL:
+                    write(ws, responseTransactionPoolMsg());
+                    break;
+                case MessageType.RESPONSE_TRANSACTION_POOL:
+                    const receivedTransactions = JSONToObject(message.data);
+                    if (receivedTransactions === null) {
+                        console.log('invalid transaction received: %s', JSON.stringify(message.data));
+                        break;
+                    }
+                    receivedTransactions.forEach((transaction) => {
+                        try {
+                            handleReceivedTransaction(transaction);
+                            // if no error is thrown, transaction was indeed added to the pool
+                            // let's broadcast transaction pool
+                            broadCastTransactionPool();
+                        } catch (e) {
+                            console.log(e.message);
+                        }
+                    });
+                    break;
+            }
+            //ch5
+        } catch (e) {
+            console.log(e);
+        }
+    });
+};
+
+const write = (ws, message) => {
+    console.log('\n8. 소켓에 메세지 보낸다~');
+    ws.send(JSON.stringify(message))
+};
+
+const broadcast = (message) => sockets.forEach((socket) => write(socket, message));
+
+const queryChainLengthMsg = () => ({ 'type': MessageType.QUERY_LATEST, 'data': null });
+
+const queryAllMsg = () => ({ 'type': MessageType.QUERY_ALL, 'data': null });
+
+const responseChainMsg = () => ({
+    'type': MessageType.RESPONSE_BLOCKCHAIN, 'data': JSON.stringify(getBlockchain())
+});
+
+const responseLatestMsg = () => ({
+    'type': MessageType.RESPONSE_BLOCKCHAIN,
+    'data': JSON.stringify([getLatestBlock()])
+});
+
+
+
+//ch5
+const queryTransactionPoolMsg = () => ({
+    'type': MessageType.QUERY_TRANSACTION_POOL,
+    'data': null
+});
+
+const responseTransactionPoolMsg = () => ({
+    'type': MessageType.RESPONSE_TRANSACTION_POOL,
+    'data': JSON.stringify(getTransactionPool())
+});
+
+
+
+
+const initErrorHandler = (ws) => {
+    const closeConnection = (myWs) => {
+        console.log('connection failed to peer: ' + myWs.url);
+        sockets.splice(sockets.indexOf(myWs), 1);
+    };
+    ws.on('close', () => closeConnection(ws));
+    ws.on('error', () => closeConnection(ws));
+};
+
+const handleBlockchainResponse = (receivedBlocks) => {
+    console.log('\n9. 메세지타입 RESPONSE_BLOCKCHAIN 의 자세한 내용들');
+
+    if (receivedBlocks.length === 0) {
+        console.log('received block chain size of 0');
+        return;
+    }
+    const latestBlockReceived = receivedBlocks[receivedBlocks.length - 1];
+    if (!isValidBlockStructure(latestBlockReceived)) {
+        console.log('block structuture not valid');
+        return;
+    }
+    const latestBlockHeld = getLatestBlock();
+    if (latestBlockReceived.index > latestBlockHeld.index) {
+        console.log('blockchain possibly behind. We got: '
+            + latestBlockHeld.index + ' Peer got: ' + latestBlockReceived.index);
+        if (latestBlockHeld.hash === latestBlockReceived.previousHash) {
+            if (addBlockToChain(latestBlockReceived)) {
+                broadcast(responseLatestMsg());
+            }
+        } else if (receivedBlocks.length === 1) {
+            console.log('We have to query the chain from our peer');
+            broadcast(queryAllMsg());
+        } else {
+            console.log('Received blockchain is longer than current blockchain');
+            replaceChain(receivedBlocks);
+        }
+    } else {
+        console.log('received blockchain is not longer than received blockchain. Do nothing');
+    }
+};
+
+const broadcastLatest = () => {
+    console.log("\n6.애드블럭 유효성 검사 끝난 후 브로드캐스트 중");
+    broadcast(responseLatestMsg());
+};
+
+const connectToPeers = (newPeer) => {
+    console.log("2. 컨넥트투피어진입");
+    console.log(newPeer);
+    const ws = new WebSocket(newPeer);
+    ws.on('open', () => {
+        console.log('3. ws.on(접속) - 오픈 진입\n');
+        initConnection(ws);
+    });
+    ws.on('error', (errorType) => {
+        console.log('connection failed' + errorType);
+    });
+};
+
+//ch5
+const broadCastTransactionPool = () => {
+    broadcast(responseTransactionPoolMsg());
+};
+
+module.exports = { connectToPeers, broadcastLatest, initP2PServer, getSockets, broadCastTransactionPool };
---
+++ naivecoinJS/chapter4_5/transaction.js
@@ -0,0 +1,377 @@
+const CryptoJS = require('crypto-js');
+const ecdsa = require('elliptic');
+const _ = require('lodash');
+
+const ec = new ecdsa.ec('secp256k1');
+
+//코인베이스 트랜잭션 : 오직 아웃풋만을 포함, 노드의 첫 트랜잭션
+//코인베이스 아웃풋의 양
+const COINBASE_AMOUNT = 50;
+
+class UnspentTxOut {
+    constructor(txOutId, txOutIndex, address, amount) {
+        this.txOutId = txOutId;
+        this.txOutIndex = txOutIndex;
+        this.address = address;
+        this.amount = amount;
+    }
+}
+
+//항상 unspent transaction outputs을 참조
+class TxIn {
+    constructor(txOutId, txOutIndex, signature) {
+        this.txOutId = txOutId;
+        this.txOutIndex = txOutIndex;
+        this.signature = signature;
+    }
+}
+
+class TxOut {
+    constructor(address, amount) {
+        this.address = address;
+        this.amount = amount;
+    }
+}
+
+class Transaction {
+    constructor(id, txIns, txOuts) {
+        this.id = id;
+        this.txIns = txIns;
+        this.txOuts = txOuts;
+    }
+}
+
+const getTransactionId = (transaction) => {
+    console.log('\n3. 트랜잭션아이디를 코인베이스 트랜잭션의 아이디에 담는다. \n or 9-1-1. 코인베이스 트랜잭션의 아이디가 유효한 ID인지 확인 ');
+    const txInContent = transaction.txIns
+        .map((txIn) => txIn.txOutId + txIn.txOutIndex)
+        .reduce((a, b) => a + b, '');
+
+    const txOutContent = transaction.txOuts
+        .map((txOut) => txOut.address + txOut.amount)
+        .reduce((a, b) => a + b, '');
+
+    return CryptoJS.SHA256(txInContent + txOutContent).toString();
+};
+
+const validateTransaction = (transaction, aUnspentTxOuts) => {
+
+    // 유효한 트랜잭션 ID 인지 확인
+    if (getTransactionId(transaction) !== transaction.id) {
+        console.log('invalid tx id: ' + transaction.id);
+        return false;
+    }
+    const hasValidTxIns = transaction.txIns
+        .map((txIn) => validateTxIn(txIn, transaction, aUnspentTxOuts))
+        .reduce((a, b) => a && b, true);
+
+    if (!hasValidTxIns) {
+        console.log('some of the txIns are invalid in tx: ' + transaction.id);
+        return false;
+    }
+
+    //아웃풋의 코인 갯수와 인풋의 코인 갯수도 같은지 확인
+    const totalTxInValues = transaction.txIns
+        .map((txIn) => getTxInAmount(txIn, aUnspentTxOuts))
+        .reduce((a, b) => (a + b), 0);
+
+    const totalTxOutValues = transaction.txOuts
+        .map((txOut) => txOut.amount)
+        .reduce((a, b) => (a + b), 0);
+
+    if (totalTxOutValues !== totalTxInValues) {
+        console.log('totalTxOutValues !== totalTxInValues in tx: ' + transaction.id);
+        return false;
+    }
+
+    return true;
+};
+
+const validateBlockTransactions = (aTransactions, aUnspentTxOuts, blockIndex) => {
+    console.log('\n9.validateBlockTransactions 진입');
+    const coinbaseTx = aTransactions[0];
+
+    if (!validateCoinbaseTx(coinbaseTx, blockIndex)) {
+        console.log('invalid coinbase transaction: ' + JSON.stringify(coinbaseTx));
+        return false;
+    }
+
+    //check for duplicate txIns. Each txIn can be included only once
+    const txIns = _(aTransactions)
+        .map(tx => tx.txIns)
+        .flatten()
+        .value();
+
+    if (hasDuplicates(txIns)) {
+        return false;
+    }
+
+    // all but coinbase transactions
+    const normalTransactions = aTransactions.slice(1);
+    return normalTransactions.map((tx) => validateTransaction(tx, aUnspentTxOuts))
+        .reduce((a, b) => (a && b), true);
+
+};
+
+const hasDuplicates = (txIns) => {
+    const groups = _.countBy(txIns, (txIn) => txIn.txOutId + txIn.txOutIndex);
+    return _(groups)
+        .map((value, key) => {
+            if (value > 1) {
+                console.log('duplicate txIn: ' + key);
+                return true;
+            } else {
+                return false;
+            }
+        })
+        .includes(true);
+};
+
+//코인베이스 트랜잭션 유효성 검사
+const validateCoinbaseTx = (transaction, blockIndex) => {
+    console.log('\n9-1. validateCoinbaseTx 진입');
+    if (transaction == null) {
+        console.log('the first transaction in the block must be coinbase transaction');
+        return false;
+    }
+    if (getTransactionId(transaction) !== transaction.id) {
+        console.log('invalid coinbase tx id: ' + transaction.id);
+        return false;
+    }
+    if (transaction.txIns.length !== 1) {
+        console.log('one txIn must be specified in the coinbase transaction');
+        return;
+    }
+    if (transaction.txIns[0].txOutIndex !== blockIndex) {
+        console.log('the txIn signature in coinbase tx must be the block height');
+        return false;
+    }
+    if (transaction.txOuts.length !== 1) {
+        console.log('invalid number of txOuts in coinbase transaction');
+        return false;
+    }
+    if (transaction.txOuts[0].amount !== COINBASE_AMOUNT) {
+        console.log('invalid coinbase amount in coinbase transaction');
+        return false;
+    }
+    console.log('\n9-2. validateCoinbaseTx 통과');
+    return true;
+};
+
+//txIns의 서명도 사용되지 않은 아웃풋을 잘 참조하고 있는지 확인
+const validateTxIn = (txIn, transaction, aUnspentTxOuts) => {
+    const referencedUTxOut =
+        aUnspentTxOuts.find((uTxO) => uTxO.txOutId === txIn.txOutId && uTxO.txOutIndex === txIn.txOutIndex);
+    if (referencedUTxOut == null) {
+        console.log('referenced txOut not found: ' + JSON.stringify(txIn));
+        return false;
+    }
+    const address = referencedUTxOut.address;
+
+    const key = ec.keyFromPublic(address, 'hex');
+    //chapter3
+    //return key.verify(transaction.id, txIn.signature);
+
+    //chapter4
+    const validSignature = key.verify(transaction.id, txIn.signature);
+    if (!validSignature) {
+        console.log('invalid txIn signature: %s txId: %s address: %s', txIn.signature, transaction.id, referencedUTxOut.address);
+        return false;
+    }
+    return true;
+
+};
+
+const getTxInAmount = (txIn, aUnspentTxOuts) => {
+    return findUnspentTxOut(txIn.txOutId, txIn.txOutIndex, aUnspentTxOuts).amount;
+};
+
+const findUnspentTxOut = (transactionId, index, aUnspentTxOuts) => {
+    console.log('\n?.findUnspentTxOut 진입');
+    return aUnspentTxOuts.find((uTxO) => uTxO.txOutId === transactionId && uTxO.txOutIndex === index);
+};
+
+//코인베이스 트랜잭션 : 노드의 첫 트랜잭션
+const getCoinbaseTransaction = (address, blockIndex) => {
+    console.log("\n2. 코인베이스 트랜잭션 진입");
+    const t = new Transaction();
+    const txIn = new TxIn();
+    txIn.signature = "";
+    txIn.txOutId = "";
+    txIn.txOutIndex = blockIndex;
+
+    t.txIns = [txIn];
+    t.txOuts = [new TxOut(address, COINBASE_AMOUNT)];
+    t.id = getTransactionId(t);
+    return t;
+};
+
+const signTxIn = (transaction, txInIndex,
+    privateKey, aUnspentTxOuts) => {
+    const txIn = transaction.txIns[txInIndex];
+
+    const dataToSign = transaction.id;
+    const referencedUnspentTxOut = findUnspentTxOut(txIn.txOutId, txIn.txOutIndex, aUnspentTxOuts);
+    if (referencedUnspentTxOut == null) {
+        console.log('could not find referenced txOut');
+        throw Error();
+    }
+    const referencedAddress = referencedUnspentTxOut.address;
+
+    if (getPublicKey(privateKey) !== referencedAddress) {
+        console.log('trying to sign an input with private' +
+            ' key that does not match the address that is referenced in txIn');
+        throw Error();
+    }
+    const key = ec.keyFromPrivate(privateKey, 'hex');
+    const signature = toHexString(key.sign(dataToSign).toDER());
+
+    return signature;
+};
+
+const updateUnspentTxOuts = (aTransactions, aUnspentTxOuts) => {
+    console.log('\n5.프로세스트랜잭션 통과후 updateUnspentTxOuts 진입');
+    console.log('뉴트랜잭션', aTransactions);
+    console.log('aUnspentTxOuts', aUnspentTxOuts);
+    const newUnspentTxOuts = aTransactions
+        .map((t) => {
+            return t.txOuts.map((txOut, index) => new UnspentTxOut(t.id, index, txOut.address, txOut.amount));
+        })
+        .reduce((a, b) => a.concat(b), []);
+
+    const consumedTxOuts = aTransactions
+        .map((t) => t.txIns)
+        .reduce((a, b) => a.concat(b), [])
+        .map((txIn) => new UnspentTxOut(txIn.txOutId, txIn.txOutIndex, '', 0));
+
+    const resultingUnspentTxOuts = aUnspentTxOuts
+        .filter(((uTxO) => !findUnspentTxOut(uTxO.txOutId, uTxO.txOutIndex, consumedTxOuts)))
+        .concat(newUnspentTxOuts);
+
+    return resultingUnspentTxOuts;
+};
+
+const processTransactions = (aTransactions, aUnspentTxOuts, blockIndex) => {
+    console.log('\n5.프로세스트랜잭션 진입');
+    if (!isValidTransactionsStructure(aTransactions)) {
+        return null;
+    }
+
+    if (!validateBlockTransactions(aTransactions, aUnspentTxOuts, blockIndex)) {
+        console.log('invalid block transactions');
+        return null;
+    }
+    return updateUnspentTxOuts(aTransactions, aUnspentTxOuts);
+};
+
+const toHexString = (byteArray) => {
+    return Array.from(byteArray, (byte) => {
+        return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('');
+};
+
+const getPublicKey = (aPrivateKey) => {
+    return ec.keyFromPrivate(aPrivateKey, 'hex').getPublic().encode('hex');
+};
+
+const isValidTxInStructure = (txIn) => {
+    console.log("\n7-1. isValidTxInStructure 진입");
+    if (txIn == null) {
+        console.log('txIn is null');
+        return false;
+    } else if (typeof txIn.signature !== 'string') {
+        console.log('invalid signature type in txIn');
+        return false;
+    } else if (typeof txIn.txOutId !== 'string') {
+        console.log('invalid txOutId type in txIn');
+        return false;
+    } else if (typeof txIn.txOutIndex !== 'number') {
+        console.log('invalid txOutIndex type in txIn');
+        return false;
+    } else {
+        console.log("\n7-2. isValidTxInStructure 통과");
+        return true;
+    }
+};
+
+const isValidTxOutStructure = (txOut) => {
+    console.log('\n7-3. isValidTxOutStructure 진입');
+    if (txOut == null) {
+        console.log('txOut is null');
+        return false;
+    } else if (typeof txOut.address !== 'string') {
+        console.log('invalid address type in txOut');
+        return false;
+    } else if (!isValidAddress(txOut.address)) {
+        console.log('invalid TxOut address');
+        return false;
+    } else if (typeof txOut.amount !== 'number') {
+        console.log('invalid amount type in txOut');
+        return false;
+    } else {
+        console.log('\n7-4. isValidTxOutStructure 통과');
+        return true;
+    }
+};
+
+const isValidTransactionsStructure = (transactions) => {
+    console.log("\n6. isValidTransactionsStructure(복수) 진입");
+    return transactions
+        .map(isValidTransactionStructure)
+        .reduce((a, b) => (a && b), true);
+};
+
+//check all members of class
+const isValidTransactionStructure = (transaction) => {
+    console.log('\n7. isValidTransactionStructure(단수) 진입');
+
+    if (typeof transaction.id !== 'string') {
+        console.log('transactionId missing');
+        return false;
+    }
+    if (!(transaction.txIns instanceof Array)) {
+        console.log('invalid txIns type in transaction');
+        return false;
+    }
+    if (!transaction.txIns
+        .map(isValidTxInStructure)
+        .reduce((a, b) => (a && b), true)) {
+        return false;
+    }
+
+    if (!(transaction.txOuts instanceof Array)) {
+        console.log('invalid txIns type in transaction');
+        return false;
+    }
+
+    if (!transaction.txOuts
+        .map(isValidTxOutStructure)
+        .reduce((a, b) => (a && b), true)) {
+        return false;
+    }
+    console.log('\n8. isValidTransactionStructure(단수) 통과');
+    return true;
+};
+
+//valid address is a valid ecdsa public key in the 04 + X-coordinate + Y-coordinate format
+const isValidAddress = (address) => {
+    console.log("\n7-3-1.isValidAddress 진입 \n or 2. 보낼 주소가 유효한 주소인지 확인");
+    if (address.length !== 130) {
+        console.log('invalid public key length');
+        return false;
+    } else if (address.match('^[a-fA-F0-9]+$') === null) {
+        console.log('public key must contain only hex characters');
+        return false;
+    } else if (!address.startsWith('04')) {
+        console.log('public key must start with 04');
+        return false;
+    }
+    console.log("\n7-3-2.isValidAddress 통과");
+    return true;
+};
+
+module.exports = {
+    processTransactions, signTxIn, getTransactionId, isValidAddress,
+    UnspentTxOut, TxIn, TxOut, getCoinbaseTransaction, getPublicKey,
+    Transaction, validateTransaction
+}
